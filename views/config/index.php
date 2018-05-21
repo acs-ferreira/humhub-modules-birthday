9afa22296f3ad669a311614cec24736729eb5ab6
Carlos Ferreira <c.ferreira@acsiiweb.com>
2018-05-21 16:38:43 +0200

    Update index.php

--- views/config/index.php
+++ views/config/index.php
@@ -1,36 +1,39 @@
 <?php
 
+use Yii;
 use yii\helpers\Html;
 use yii\helpers\Url;
 use humhub\compat\CActiveForm;
 ?>
+
 <div class="panel panel-default">
-    <div class="panel-heading"><?php echo Yii::t('BirthdayModule.base', 'Birthday Module Configuration'); ?></div>
+    <div class="panel-heading">
+        <?= Yii::t('BirthdayModule.base', 'Birthday Module Configuration'); ?>
+    </div>
+    
     <div class="panel-body">
-
-
-        <p><?php echo Yii::t('BirthdayModule.base', 'You may configure the number of days within the upcoming birthdays are shown.'); ?></p>
-        <br/>
+        <p><?= Yii::t('BirthdayModule.base', 'You may configure the number of days within the upcoming birthdays are shown.'); ?></p>
+        <br>
 
         <?php $form = CActiveForm::begin(); ?>
 
-        <?php echo $form->errorSummary($model); ?>
+        <?= $form->errorSummary($model); ?>
 
         <div class="form-group">
-            <?php echo $form->labelEx($model, 'shownDays'); ?>
-            <?php echo $form->textField($model, 'shownDays', array('class' => 'form-control')); ?>
-            <?php echo $form->error($model, 'shownDays'); ?>
+            <?= $form->labelEx($model, 'shownDays'); ?>
+            <?= $form->textField($model, 'shownDays', ['class' => 'form-control']); ?>
+            <?= $form->error($model, 'shownDays'); ?>
         </div>
 
         <div class="form-group">
-            <?php echo $form->labelEx($model, 'excludedGroup'); ?>
-            <?php echo $form->textField($model, 'excludedGroup', array('class' => 'form-control')); ?>
-            <?php echo $form->error($model, 'excludedGroup'); ?>
+            <?= $form->labelEx($model, 'excludedGroup'); ?>
+            <?= $form->textField($model, 'excludedGroup', ['class' => 'form-control']); ?>
+            <?= $form->error($model, 'excludedGroup'); ?>
         </div>
 
         <hr>
-        <?php echo Html::submitButton(Yii::t('BirthdayModule.base', 'Save'), array('class' => 'btn btn-primary')); ?>
-        <a class="btn btn-default" href="<?php echo Url::to(['/admin/module']); ?>"><?php echo Yii::t('BirthdayModule.base', 'Back to modules'); ?></a>
+        <?= Html::submitButton(Yii::t('BirthdayModule.base', 'Save'), ['class' => 'btn btn-primary']); ?>
+        <a class="btn btn-default" href="<?= Url::to(['/admin/module']); ?>"><?= Yii::t('BirthdayModule.base', 'Back to modules'); ?></a>
 
         <?php CActiveForm::end(); ?>
     </div>
